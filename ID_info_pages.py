01854ae2c5e30aee91c305e43a4ec1c4afadb6bd
Chris <66043010+poheniks@users.noreply.github.com>
2025-11-02 16:11:04 -0700

    Added back ID files to fix an import loop between items.py and constants.py

---
+++ ID_info_pages.py
@@ -0,0 +1,14 @@
+ip_reference_buildings = 0
+ip_reference_orders = 1
+ip_morale = 2
+ip_economy = 3
+ip_courtship = 4
+ip_politics = 5
+ip_character_backgrounds = 6
+ip_military_campaigns = 7
+ip_pendor_history = 8
+ip_pendor_present = 9
+ip_pendor_orders = 10
+ip_pendor_althea = 11
+
+
