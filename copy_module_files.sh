cd0f5fea3c785ccfa80b2f113d982df5216a2d57
Chris <66043010+poheniks@users.noreply.github.com>
2025-11-22 18:11:01 -0700

    Modified recruit companions cheat for player companions

---
+++ copy_module_files.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+set MB_PATH ~/.local/share/Steam/steamapps/common/MountBlade\ Warband/Modules/pendor_battletime/
+cd build
+echo Copying files to $MB_PATH
+cp -rf * $MB_PATH
+
+
